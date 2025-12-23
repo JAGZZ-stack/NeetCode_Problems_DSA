4274f8c8816ec47979f8b397d8d35c07e31eaa24
JAGZZ-stack <jagshwaran20@gmail.com>
2025-12-23 23:51:41 +0530

    Update 3_Sliding Window.cpp

--- 3_Sliding Window.cpp
+++ 3_Sliding Window.cpp
@@ -112,7 +112,7 @@ public:
 // TC : O(nums.size())
 // SC : O(k); // The deque stores atmost k elements. We can ignore considering the space for maxList
 
-5. Count the occurences of the anagrams 
+Problem 4. Count the occurences of the anagrams 
 
 // Input : text = "forxxorfxdofr", pattern = "for"
 // Output : 3
@@ -155,3 +155,65 @@ class Solution {
 
 // TC : O(pat.size() + text.size())
 // SC : O(pat.size())
+
+// Discussion - Form 2 :
+// ---------------------
+Problem 1. Find the longest subarray of all 1s with atmost k flips. 
+
+// Longest - ask (It could be longest, shortest, count, sum of length).
+// Subarray - what needs to be found (It could be subarray, max - min (gap)).
+// atmost k flips - criteria (It could be any monotonic criteria) 
+// Criteria - No. of 0s in (L -> R) <= k
+//          - Also true for (L+1 -> R) <= k
+//          - Criteria proved monotonic.
+
+// Brute Force is O(n ^ 2).
+
+// [0, 1, 0, 1, 0, 0, 1, 1, 0] 
+// Head = -1; Tail = 0; Because we need the initial length to be zero.
+// Think of a snake, it moves forward (head) and eats as many elements as it can.
+
+// A. Template (TC : O(n))
+// 1. For each position of tail, as long as it can contain atmost k 0s, keep incrementing the head unless you reach the end.
+// 2. Calculate your answer for the range [Tail, Head].
+// 3. For next iteration, move Tail one step ahead.
+
+// B. Decide the Data structure.
+// This is needed in order to quickly check if we can eat the next element or not.
+// Here, a variable count should be enough for this problem.
+
+// C. Decide the Answer :
+// Here, max length is the ans. 
+// So, ans = max(ans, Head - Tail + 1)
+
+int solve(int[] nums, int k) {
+    int head = -1; 
+    int tail = 0;
+    int count = 0; 
+    int sol = 0;
+    
+    while(tail < nums.size() {
+        // Step 1 : Keep eating as much as you can.
+        while(head+1 < nums.size() && (count < k || (count == k && nums[head+1] == 1)) {
+            head++;
+            if(nums[head] == 0) count++;
+        }
+
+        // Step 2 : Update the answer
+        sol = max(sol, head - tail + 1);
+
+        // Step 3 : Move tail one step forward.
+        if(tail <= head) {
+            if(nums[tail] == 0) count--;
+            tail++;
+        } else {
+            tail ++;
+            head = tail - 1;
+            count = 0; // Resetting the data structure.
+        }
+    }
+    return sol;
+}
+
+// TC : O(nums.size())
+// SC : O(1)
