459a169840659255bd63eab85b9beb7df8f6c715
JAGZZ-stack <jagshwaran20@gmail.com>
2025-12-26 20:49:37 +0530

    Update 1_Arrays and Hashing.cpp

--- 1_Arrays and Hashing.cpp
+++ 1_Arrays and Hashing.cpp
@@ -371,7 +371,53 @@ void differenceArray(const vector<int>& nums, int k, int x) {
 } // TC : O(N + K); SC : O(N)
 
 =========================================================================================================================
+Problem 15. Range Sum Query 2D Immutable 
 
+class NumMatrix {
+private:
+    // Move this to private scope so all methods can see it
+    vector<vector<int>> prefixSum;
+
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        // Construction of prefix sum matrix
+        int rows = matrix.size();
+        int columns = matrix[0].size();
+
+        // Initialize the member variable
+        prefixSum.assign(rows, vector<int>(columns, 0));
+
+        for(int i = 0; i < rows; i++) {
+            for(int j = 0; j < columns; j++) {
+                prefixSum[i][j] += matrix[i][j];
+                if(i > 0) prefixSum[i][j] += prefixSum[i-1][j];
+                if(j > 0) prefixSum[i][j] += prefixSum[i][j-1];
+                if(i > 0 && j > 0) prefixSum[i][j] -= prefixSum[i-1][j-1];
+            }
+        }
+        
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int rectangleSum = prefixSum[row2][col2];
+        if(row1 > 0) rectangleSum -= prefixSum[row1-1][col2];
+        if(col1 > 0) rectangleSum -= prefixSum[row2][col1-1];
+        if(row1 > 0 && col1 > 0) rectangleSum += prefixSum[row1-1][col1-1];
+
+        return rectangleSum;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
+
+// Complexity Check :
+// Construction (Time): O(rows * columns);
+// Queries (Time) : O(1)
+// (Space) : O(rows * columns)
 
 Problem 21. Subarray Sum Equals K // (Assume the input might contain negative numbers also)
 // Prefix Sum + Hashmap
